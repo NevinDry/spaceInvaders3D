a9defd12ae572968a561e123ba4e8aafac837c62
Nevindry <puissant.etienne@gmail.com>
2014-10-21 11:56:36 +0200

    Score sort OK

---
+++ score/score.php
@@ -0,0 +1,34 @@
+<?php
+if(isset($_GET["name"]))
+{
+	$name = $_GET["name"];
+}
+if(isset($_GET["score"]))
+{
+	$score = $_GET["score"];
+}
+
+$json = file_get_contents("score.json");
+
+$parsed_json = json_decode($json);
+
+print_r($parsed_json);
+
+$parsed_json->$name = $score;
+
+print_r($parsed_json);
+
+$scoreJsonEncoded = json_encode($parsed_json);
+print_r($scoreJsonEncoded);
+
+$emptyFile = fopen('score.json',"w");
+ftruncate($emptyFile,0);
+fclose($emptyFile);
+
+if(!$fillFile = fopen('score.json', 'a')){
+	exit;
+} else {
+	fwrite($fillFile, $scoreJsonEncoded);
+	fclose($fillFile);
+}
+?>
